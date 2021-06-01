b390bd127023f58ba94c006934bd768bdbd4c8ca
vkddnrs <vkddnrs@gmail.com>
2021-06-01 10:49:39 +0300

    Анимация перезарядки 2  - anim notify.

---
+++ Source/ShooterGame/Public/Animations/BaseAnimNotify.h
@@ -0,0 +1,17 @@
+// ShooterGame. All Right Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimNotifies/AnimNotify.h"
+#include "BaseAnimNotify.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SHOOTERGAME_API UBaseAnimNotify : public UAnimNotify
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/ShooterGame/Public/Animations/ReloadFinishedAnimNotify.h
@@ -0,0 +1,17 @@
+// ShooterGame. All Right Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animations/BaseAnimNotify.h"
+#include "ReloadFinishedAnimNotify.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SHOOTERGAME_API UReloadFinishedAnimNotify : public UBaseAnimNotify
+{
+	GENERATED_BODY()
+	
+};
