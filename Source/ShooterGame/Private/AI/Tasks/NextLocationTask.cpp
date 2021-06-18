cb9034ddf6188a32110a601e19553614c0875a14
vkddnrs <vkddnrs@gmail.com>
2021-06-18 15:22:19 +0300

    Небольшой рефакторинг

--- Source/ShooterGame/Private/AI/Tasks/NextLocationTask.cpp
+++ Source/ShooterGame/Private/AI/Tasks/NextLocationTask.cpp
@@ -13,11 +13,11 @@ UNextLocationTask::UNextLocationTask()
 
 EBTNodeResult::Type UNextLocationTask::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
 {
-    const auto AIController = OwnerComp.GetAIOwner();
+    const auto Controller = OwnerComp.GetAIOwner();
     const auto Blackboard = OwnerComp.GetBlackboardComponent();
-    const auto Pawn = AIController->GetPawn();
+    const auto Pawn = Controller->GetPawn();
     const auto NavSystem = UNavigationSystemV1::GetCurrent(Pawn); 
-    if(!ensure(AIController || Blackboard || Pawn || NavSystem)) return EBTNodeResult::Failed;
+    if(!ensure(Controller || Blackboard || Pawn || NavSystem)) return EBTNodeResult::Failed;
 
     FNavLocation NavLocation;
     auto Location = Pawn->GetActorLocation();
